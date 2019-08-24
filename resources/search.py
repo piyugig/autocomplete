9a321f43f46b4c16baf8df8c2f61929e3b901928
Uzair Nizamuddin <uzair_y3k@live.com>
2019-08-24 13:24:36 +0200

    added redissearch

--- resources/search.py
+++ resources/search.py
@@ -2,12 +2,19 @@ from flask import request
 from flask_restful import Resource
 import time
 import json
+from redisearch import TextField, NumericField, Query, AutoCompleter, Suggestion
 
 
 class Search(Resource):
     def get(self, keyword):
-        data = {'status': 'OK'}
-        return data, 200
+        ac = AutoCompleter('ac', 'redis-search')
+        ac.add_suggestions(
+            Suggestion('google', 5.0),
+            Suggestion('goo', 1.0)
+        )
+        res = ac.get_suggestions('goo')
+        res = ac.get_suggestions('goo', fuzzy = True)
+        return res, 200
 
 class Add(Resource):
     def post(self):
